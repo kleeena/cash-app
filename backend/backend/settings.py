cf6fa955473acca49b7ab6afaa1fb1903a9c2984
kleeena <62442533+kleeena@users.noreply.github.com>
2025-03-15 15:09:35 +0500

    Settings Updated

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-^e@(5#a*j#&9%13+u3t3fnpeetto!04#ewqkroz6pq=*)d)sg&
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1', '.vercel.app']
 
 
 # Application definition
