fbec0abd468533caef79cc69b3cf1781c4f42098
kleeena <62442533+kleeena@users.noreply.github.com>
2025-03-20 02:13:43 +0500

    Update vite.config.js

--- cashapp_frontend/vite.config.js
+++ cashapp_frontend/vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "http://localhost:8000"
+  base: process.env.VITE_API_BASE_URL
 })
