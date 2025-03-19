5227b5376db6bd51ae8cc5a50d372ddd7dd1d6e3
kleeena <62442533+kleeena@users.noreply.github.com>
2025-03-20 02:32:30 +0500

    Update vite.config.js

--- cashapp_frontend/vite.config.js
+++ cashapp_frontend/vite.config.js
@@ -4,5 +4,13 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: process.env.VITE_API_BASE_URL
+  base: process.env.VITE_API_BASE_URL,
+  server: {
+    proxy: {
+      '/api': {
+        target: 'process.env.VITE_API_BASE_URL', // Change this to your backend URL
+        changeOrigin: true,
+      }
+    }
+  }
 })
